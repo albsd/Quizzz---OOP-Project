334fd8c02c1259f13ee21c01d98ebd15e0d1992a
Pil Kyu Cho <p.k.cho@student.tudelft.nl>
2022-02-27 11:29:22 +0100

    fix pipeline

--- client/src/main/java/client/scenes/AddQuoteCtrl.java
+++ client/src/main/java/client/scenes/AddQuoteCtrl.java
@@ -20,6 +20,7 @@ import com.google.inject.Inject;
 import client.utils.ServerUtils;
 import commons.Person;
 import commons.Quote;
+import jakarta.ws.rs.WebApplicationException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
--- client/src/main/java/client/scenes/QuoteOverviewCtrl.java
+++ client/src/main/java/client/scenes/QuoteOverviewCtrl.java
@@ -22,6 +22,7 @@ import client.utils.ServerUtils;
 import commons.Quote;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
