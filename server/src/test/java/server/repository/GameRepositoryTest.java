c4dff44fe234ccb9634c38b0b54ae8a79779e52e
Elias Hoste <E.R.Hoste@student.tudelft.nl>
2022-03-02 16:11:25 +0100

    fix missing import

--- server/src/test/java/server/repository/GameRepositoryTest.java
+++ server/src/test/java/server/repository/GameRepositoryTest.java
@@ -10,6 +10,7 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class GameRepositoryTest {
 
