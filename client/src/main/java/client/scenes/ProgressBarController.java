b13423204b5b8ba63e9e0f93621013955a123984
Albert Sandu <a.a.sandu@student.tudelft.nl>
2022-03-06 11:22:39 +0100

    disable buttons when question time is over

--- client/src/main/java/client/scenes/ProgressBarController.java
+++ client/src/main/java/client/scenes/ProgressBarController.java
@@ -1,115 +1,70 @@
 package client.scenes;
 
+//import javafx.application.Platform;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+//import javafx.scene.control.Button;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import java.net.URL;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class ProgressBarController {
+//import java.util.Timer;
+//import java.util.TimerTask;
+
+public class ProgressBarController implements Initializable {
     @FXML
     private ProgressBar bar = new ProgressBar(1);
 
     @FXML
-    private Button halveButton;
-
-    @FXML
-    private Button startButton;
-
-    @FXML
     private Label label;
 
-    private final double maxTime = 20000;
-    private final double oneSecond = 1000;
-    private final double decrement = 25;    // 25ms
+    @FXML
+    private Button option1;
 
-    private double currentTime = maxTime;
-    private boolean started = false;
+    @FXML
+    private Button option2;
 
-    private Timer timer = new Timer();
-    private TimerTask currentTask;
+    @FXML
+    private Label verifLabel;
 
-    private TimerTask newTimerTask() {
-        return new TimerTask() {
-            public void run() {
-                currentTime -= decrement;
-                if (currentTime <= 0) {
-                    Platform.runLater(() -> label.setText("Time's over!"));
-                    currentTime = 0;
-                    cancel();
-                } else {
-                    Platform.runLater(() -> label.setText(
-                            String.format("%.2f", currentTime / oneSecond)));
-                    Platform.runLater(() ->
-                            bar.setProgress(currentTime / maxTime));
-                }
-            }
-        };
-    }
+    private QuestionTimer questionTimer = new QuestionTimer();
 
-    /*private TimerTask task = new TimerTask() {
-        public void run() {
-            currentTime -= decrement;
-            if (currentTime <= 0) {
-                Platform.runLater(() -> label.setText("Time's over!"));
-                currentTime = 0;
-                task.cancel();
-            }
-            else{
-                Platform.runLater(() -> label.setText(
-                        String.format("%.2f", currentTime / oneSecond)));
-                Platform.runLater(() -> bar.setProgress(currentTime / maxTime));
-            }
-        }
-    };*/
 
-    public double getCurrentTime() {
-        return currentTime;
-    }
-
-    public void start() {
-        if (started) {
-            System.out.println("Already started! Reset first.");
-        } else {
-            started = true;
-            final int delay = 25;
-            final int period = 25;
-            currentTask = newTimerTask();
-            timer.scheduleAtFixedRate(currentTask, delay, period);
-        }
-    }
-
-    public Timer getTimer() {
-        return timer;
-    }
-
-    public TimerTask getTask() {
-        return currentTask;
+    public void initialize(final URL location, final ResourceBundle resources) {
+        start();
     }
 
     public void halve() {
-        if (started) {
-            if (currentTime <= 0) {
-                System.out.println("Already finished!");
-            } else {
-                currentTime /= 2;
-            }
-        } else {
-            System.out.println("Not started yet!");
-        }
+        questionTimer.halve();
     }
 
     public void reset() {
-        currentTask.cancel();
-        started = false;
-        currentTime = maxTime;
+        questionTimer.reset();
         Platform.runLater(() ->
-                label.setText(String.valueOf(currentTime / oneSecond)));
+                label.setText(String.valueOf(questionTimer.getCurrentTime()
+                        / questionTimer.getOneSecond())));
         Platform.runLater(() ->
-                bar.setProgress(currentTime / maxTime));
+                bar.setProgress(questionTimer.getCurrentTime()
+                        / questionTimer.getMaxTime()));
+    }
+
+    public void start() {
+        List<Button> buttons = new ArrayList<>();
+        buttons.add(option1);
+        buttons.add(option2);
+        questionTimer.startClientTimer(label, bar, buttons);
+    }
+
+    public void onOptionClick() throws InterruptedException {
+        Instant time = Instant.now();
+        verifLabel.setText("Option chosen at " + time);
     }
 
 }
