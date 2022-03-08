057bc1d1891e1e811c7b7035d3df90b26c81a149
Kerem Yoner <k.yoner@student.tudelft.nl>
2022-03-08 11:59:07 +0100

    Changed LobbyController to be able to navigate to game screen

--- client/src/main/java/client/scenes/LobbyController.java
+++ client/src/main/java/client/scenes/LobbyController.java
@@ -37,12 +37,12 @@ public class LobbyController {
     public void start(final ActionEvent e) throws IOException {
         //Starting the game
 
-//        FXMLLoader loader = new FXMLLoader(
-//                getClass().getResource("MultiplayerGame.fxml"));
-//        root = loader.load();
-//        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
-//        scene = new Scene(root);
-//        stage.setScene(scene);
-//        stage.show();
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("GameMultiplayer.fxml"));
+        root = loader.load();
+        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
