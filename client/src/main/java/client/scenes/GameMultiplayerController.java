68b7b9eaaa645e369ccf73c0257962d770422b9a
Kerem Yoner <k.yoner@student.tudelft.nl>
2022-03-08 11:58:39 +0100

    Added JavaFX elements to game screen

---
+++ client/src/main/java/client/scenes/GameMultiplayerController.java
@@ -0,0 +1,27 @@
+package client.scenes;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class GameMultiplayerController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void returnMenu(final ActionEvent e) throws IOException {
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("Splash.fxml"));
+        root = loader.load();
+        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
