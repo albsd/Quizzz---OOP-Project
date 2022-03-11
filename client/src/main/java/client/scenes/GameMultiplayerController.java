f2b544e480a9f62a961064713ca77b1fa8488827
Pil Kyu Cho <p.k.cho@student.tudelft.nl>
2022-03-11 16:06:14 +0100

    request for leaderboard every 5 questions

--- client/src/main/java/client/scenes/GameMultiplayerController.java
+++ client/src/main/java/client/scenes/GameMultiplayerController.java
@@ -1,9 +1,8 @@
 package client.scenes;
 
-import commons.Game;
-import commons.LobbyMessage;
-import commons.Player;
-import commons.Question;
+import client.utils.ServerUtils;
+import com.google.inject.Inject;
+import commons.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -22,6 +21,7 @@ import java.time.LocalTime;
 import java.util.ResourceBundle;
 
 import client.Main;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.util.HtmlUtils;
 
 public class GameMultiplayerController implements Initializable {
@@ -37,11 +37,19 @@ public class GameMultiplayerController implements Initializable {
     @FXML
     private Pane popupMenu;
 
-    //find way to connect a game object and player object so that
-    //we can calculate score of game timer and assign to player
-    //later score must be sent to server to display leaderboard
+    private Stage stage;
+
+    private Scene scene;
+
+    private final ServerUtils server;
+
     private Player me;
     private Game currentGame;
+
+    @Inject
+    public GameMultiplayerController(final ServerUtils server) {
+        this.server = server;
+    }
 
     @Override
     public void initialize(final URL location, final ResourceBundle resources) {
@@ -66,14 +74,14 @@ public class GameMultiplayerController implements Initializable {
     public void returnMenu(final ActionEvent e) throws IOException {
         var root = Main.FXML.load(SplashController.class, "client", "scenes", "Splash.fxml");
 
-        Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root.getValue());
+        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        scene = new Scene(root.getValue());
         stage.setScene(scene);
         stage.show();
     }
 
     //this is for multiple choice questions
-    public void checkAnswer(final ActionEvent e) {
+    public void checkMulChoiceAnswer(final ActionEvent e) {
         int correctAnswer = currentGame.getCurrentQuestion().getAnswer();
         String optionStr = ((Button)e.getSource()).getText();
         int option = Integer.parseInt(optionStr);
@@ -84,11 +92,46 @@ public class GameMultiplayerController implements Initializable {
             System.out.println("Wrong answer. No points");
         }
     }
+    //this is for open questions
+    public void checkOpenAnswer(final ActionEvent e) {
+        int correctAnswer = currentGame.getCurrentQuestion().getAnswer();
+        String optionStr = ((Button)e.getSource()).getText();
+        int option = Integer.parseInt(optionStr);
+        calculateOpenPoints(correctAnswer, option);
+    }
 
     private void calculateMulChoicePoints(){
         int base = 50;
         int bonusScore = (me.getTime() / 1000) * 2;
         me.setScore(base + bonusScore);
+    }
+
+    private void calculateOpenPoints(int answer, int option) {
+        int bonusScore = (me.getTime() / 1000) * 2;
+        int base;
+        int offPercentage = (int)Math.round(((double)Math.abs((option - answer)) / answer) * 100);
+        int accuracyPercentage = 100 - offPercentage;
+        if(accuracyPercentage==100) base = 100;
+        else if(90<=accuracyPercentage && accuracyPercentage< 99) base=90;
+        else if(80<=accuracyPercentage && accuracyPercentage< 89) base=80;
+        else if(70<=accuracyPercentage && accuracyPercentage< 79) base=70;
+        else if(60<=accuracyPercentage && accuracyPercentage< 69) base=60;
+        else if(50<=accuracyPercentage && accuracyPercentage< 59) base=50;
+        else base = 0;
+        me.setScore(base + bonusScore);
+    }
+
+    //request for leaderboard every 5 questions
+    public void getLeaderboard(ActionEvent e) {
+        int questionNum = Integer.parseInt(questionNumber.getText());
+        if (questionNum % 5 == 0) {
+            Leaderboard leaderboard = server.getLeaderboard(currentGame.getId().toString());
+            var root = Main.FXML.load(
+                    LeaderboardController.class, "client", "scenes", "Leaderboard.fxml");
+
+            stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+            scene = new Scene(root.getValue());
+        }
     }
 
     public void setMe(Player me){
