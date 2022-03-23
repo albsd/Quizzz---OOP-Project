e550edaf6bee0c2499486a02de33a63a4a67665d
Pil Kyu Cho <p.k.cho@student.tudelft.nl>
2022-03-23 23:18:46 +0100

    created server tests

---
+++ server/src/test/java/server/controller/ActivityControllerTest.java
@@ -0,0 +1,20 @@
+package server.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActivityControllerTest {
+
+    @Test
+    void getAllActivity() {
+    }
+
+    @Test
+    void addActivity() {
+    }
+
+    @Test
+    void deleteActivity() {
+    }
+}
---
+++ server/src/test/java/server/service/ActivityServiceTest.java
@@ -0,0 +1,40 @@
+package server.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActivityServiceTest {
+
+    @Test
+    void getActivities() {
+    }
+
+    @Test
+    void getQuestionList() {
+    }
+
+    @Test
+    void turnActivityIntoQuestion() {
+    }
+
+    @Test
+    void generateOptions() {
+    }
+
+    @Test
+    void addActivity() {
+    }
+
+    @Test
+    void deleteActivity() {
+    }
+
+    @Test
+    void getAllActivities() {
+    }
+
+    @Test
+    void populateRepo() {
+    }
+}
