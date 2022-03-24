09b1b09e67d8e7bb708da9c1a8077a4c234ebdb1
Pil Kyu Cho <p.k.cho@student.tudelft.nl>
2022-03-24 16:27:10 +0100

    added javadoc coments for server side

--- server/src/main/java/server/configuration/GameConfig.java
+++ server/src/main/java/server/configuration/GameConfig.java
@@ -8,12 +8,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import server.controller.AppController;
-import server.controller.GameController;
 import server.repository.GameRepository;
 import server.service.GameService;
 
 import java.util.List;
 
+/**
+ * Configuration class that runs scheduled methods.
+ * Used to clean up finished or unused game objects
+ * in game repository.
+ */
 @Configuration
 @EnableScheduling
 public class GameConfig {
@@ -26,9 +30,6 @@ public class GameConfig {
 
     @Autowired
     private AppController appCtrl;
-
-    @Autowired
-    private GameController gameCtrl;
 
     /**
      * Every 5 seconds checks the multiplaeyr games is repo.
