c46487d633714521a46ff3d8a1ec577f0db95a7c
Albert Sandu <a.a.sandu@student.tudelft.nl>
2022-03-24 19:09:02 +0100

    change open choice score calculation and add javadoc

--- commons/src/main/java/commons/FreeResponseQuestion.java
+++ commons/src/main/java/commons/FreeResponseQuestion.java
@@ -17,16 +17,11 @@ public class FreeResponseQuestion extends Question {
 
     /**
      * Calculates the score of an open answer question.
-     * If the accuracy is below a specific threshold then no points are gained.
-     * To balance the points gained from this type of question,
-     * accuracy is taken into account when calculating the bonus score.
-     * Example: a player with a 75% accuracy and with a bonus time of 40
-     * now gets roughly 88.25 points.
-     * Maximum points: 125 (75 for 100% accuracy and 50 for 100% speed & 100% accuracy).
+     * Maximum points: 125 (75 for 100% accuracy and 50 for 100% speed).
      *
      * @param option value entered by user
      * @param time how long it took the player to answer
-     * @return the player's points
+     * @return the score of the player
      */
 
     public int calculateScore(final long option, final int time) {
@@ -35,11 +30,14 @@ public class FreeResponseQuestion extends Question {
                 - (int) Math.round(((double) Math.abs((option - getAnswer())) / getAnswer()) * 100);
         final int maxScore = 75;
 
-        if (accuracyPercentage < 50) {
+        if (accuracyPercentage < 75) {
+            bonusScore /= 1.25;
+        }
+        if (accuracyPercentage < 40) {
             accuracyPercentage = 0;
             bonusScore = 0;
         }
-
-        return (maxScore * accuracyPercentage) / 100 + bonusScore / (1 + ((100 - accuracyPercentage) / 100));
+        
+        return (maxScore * accuracyPercentage) / 100 + bonusScore;
     }
 }
