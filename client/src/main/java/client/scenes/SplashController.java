479d41fe8b9c08866082dcfeed835079ab2886b1
Kerem Yoner <k.yoner@student.tudelft.nl>
2022-03-25 20:42:06 +0100

    Changed Splash screen UI.

--- client/src/main/java/client/scenes/SplashController.java
+++ client/src/main/java/client/scenes/SplashController.java
@@ -7,30 +7,36 @@ import commons.Player;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 import javax.inject.Inject;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class SplashController {
+public class SplashController implements Initializable {
 
     @FXML
     private TextField nickField;
 
     @FXML
-    private Label warning;
-
-    @FXML
-    private Label title;
+    private Label warning, title;
     
     @FXML
     private Parent popup;
 
     @FXML
-    private PopupController popupController; 
+    private PopupController popupController;
 
+    @FXML
+    private Button singleplayerButton, leaderBoardButton, multiplayerButton;
+
+    private final Font font1, font2;
     public final Color red = new Color(0.8, 0, 0, 1);
     public final Color green = new Color(0, 0.6, 0, 1);
 
@@ -42,6 +48,26 @@ public class SplashController {
     public SplashController(final ServerUtils server, final FXMLController fxml) {
         this.server = server;
         this.fxml = fxml;
+        this.font1 = Font.loadFont(getClass().getResourceAsStream("/fonts/Righteous-Regular.ttf"), 24);
+        this.font2 = Font.loadFont(getClass().getResourceAsStream("/fonts/Righteous-Regular.ttf"), 150);
+    }
+
+    /**
+     * Called to initialize a controller after its root element has been
+     * completely processed.
+     *
+     * @param location  The location used to resolve relative paths for the root object, or
+     *                  {@code null} if the location is not known.
+     * @param resources The resources used to localize the root object, or {@code null} if
+     */
+    @Override
+    public void initialize(final URL location, final ResourceBundle resources) {
+        title.setFont(font2);
+        warning.setFont(font1);
+        nickField.setFont(font1);
+        singleplayerButton.setFont(font1);
+        leaderBoardButton.setFont(font1);
+        multiplayerButton.setFont(font1);
     }
 
     @FXML
