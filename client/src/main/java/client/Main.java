1589697bd0da2a2e44bdff3f8bea7909b89d631c
Pil Kyu Cho <p.k.cho@student.tudelft.nl>
2022-04-03 00:02:57 +0200

    fix audio file stream in jar file

--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -1,29 +1,8 @@
-
 package client;
 
-import javafx.application.Application;
-import javafx.scene.text.Font;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
-import com.google.inject.Injector;
-
-import static com.google.inject.Guice.createInjector;
-
-public class Main extends Application {
-
-    private static final Injector INJECTOR = createInjector(new MyModule());
-    private static final MyFXML FXML = new MyFXML(INJECTOR);
+public class Main {
 
     public static void main(final String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(final Stage stage) throws IOException {
-        Font.loadFont(getClass().getResourceAsStream("/fonts/Righteous-Regular.ttf"), 24);
-        var mainCtrl = INJECTOR.getInstance(FXMLController.class);
-        mainCtrl.initialize(stage, FXML);
+        Launcher.main(args);
     }
 }
